4faa9a7899bd31622fc9f6aa3bd13ccc893ad69b
Juan Vila Nova Rojas Moreno <juanvnrojas@gmail.com>
2025-08-19 15:43:12 -0300

    Update EmendaIndividual.java

--- EmendaIndividual.java
+++ EmendaIndividual.java
@@ -3,9 +3,10 @@ import java.time.LocalDate;
 public class EmendaIndividual extends Emenda {
     private String tipoTransferencia;
 
-    public EmendaIndividual(int numero, double codigo, String tipo, String autoria, String ufAutoria, String objetivo,
+    public EmendaIndividual(int numero, double codigo, String tipo, String autoria, String estadoAutoria,
+            String objetivo,
             LocalDate ano, String tipoTransferencia) {
-        super(numero, codigo, tipo, autoria, ufAutoria, objetivo, ano);
+        super(numero, codigo, tipo, autoria, estadoAutoria, objetivo, ano);
         this.tipoTransferencia = tipoTransferencia;
     }
 
@@ -15,5 +16,14 @@ public class EmendaIndividual extends Emenda {
 
     public void setTipoTransferencia(String tipoTransferencia) {
         this.tipoTransferencia = tipoTransferencia;
+    }
+
+    @Override
+    public void imprimir() {
+        super.imprimir();
+        System.out.println("Tipo de Transferência: " + getTipoTransferencia());
+        System.out.printf("Valor Empenhado: R$ %,.2f\n", calcularValorPorFase("Empenho"));
+        System.out.printf("Valor Liquidado: R$ %,.2f\n", calcularValorPorFase("Liquidação"));
+        System.out.printf("Valor Pago: R$ %,.2f\n", calcularValorPorFase("Pagamento"));
     }
 }
